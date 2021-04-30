39448ec4cdc2690c06acbee78be86b9f482c1389
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-04-30 04:02:54 +0000

    add disk

---
+++ src/disk.rs
@@ -0,0 +1,53 @@
+use crate::config::ENV_CONFIG;
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, Read, Seek, SeekFrom, Write},
+    path::Path,
+};
+
+pub struct DiskManager {
+    heap_file: File,
+    next_page_id: u64,
+}
+
+impl DiskManager {
+    pub fn new(heap_file: File) -> io::Result<Self> {
+        let heap_file_size = heap_file.metadata()?.len();
+        let next_page_id = heap_file_size / ENV_CONFIG.page_size as u64;
+        Ok(Self {
+            heap_file,
+            next_page_id,
+        })
+    }
+
+    pub fn open(heap_file_path: impl AsRef<Path>) -> io::Result<Self> {
+        let heap_file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(heap_file_path)?;
+        Self::new(heap_file)
+    }
+
+    pub fn allocate_page(&mut self) -> PageId {
+        let page_id = self.next_page_id;
+        self.next_page_id += 1;
+        PageId(page_id)
+    }
+
+    pub fn write_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
+        self.seek(page_id)?;
+        self.heap_file.write_all(data)
+    }
+    pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
+        self.seek(page_id)?;
+        self.heap_file.read_exact(data)
+    }
+    fn seek(&mut self, page_id: PageId) -> io::Result<()> {
+        let offset = ENV_CONFIG.page_size * page_id.0;
+        self.heap_file.seek(SeekFrom::Start(offset))?;
+        Ok(())
+    }
+}
+
+pub struct PageId(pub u64);
